157a511ebf4186238485c15ac6d21bca23323f6c
Ryo Ota <nwtgck@gmail.com>
2019-06-11 23:26:23 +0900

    [add] Create a simple routing in an example

--- build.sbt
+++ build.sbt
@@ -3,3 +3,25 @@ name := "gh-card"
 version := "0.1.0-SNAPSHOT"
 
 scalaVersion := "2.11.12"
+
+val akkaHttpVersion = "10.0.15"
+
+lazy val root = (project in file(".")).
+  settings(
+    libraryDependencies ++= Seq(
+      // sbt from http://akka.io/docs/
+      "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test,
+
+      // Slick
+      "com.typesafe.slick" %% "slick" % "3.1.1",
+      "org.slf4j" % "slf4j-nop" % "1.7.26",
+      "com.h2database" % "h2" % "1.4.199",
+
+      // scopt
+      "com.github.scopt" %% "scopt" % "3.6.0",
+
+      // ScalaTest
+      "org.scalatest" %% "scalatest" % "3.0.7" % Test
+    )
+  )
--- src/main/scala/io/github/nwtgck/gh_card/Main.scala
+++ src/main/scala/io/github/nwtgck/gh_card/Main.scala
@@ -1,7 +1,33 @@
 package io.github.nwtgck.gh_card
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("hello, world")
+    implicit val system: ActorSystem = ActorSystem("trans-server")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+
+    implicit val executionContext = system.dispatcher
+
+    val route = {
+      import akka.http.scaladsl.server.Directives._
+      import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+      get {
+        pathSingleSlash {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<html><body>Hello world!</body></html>"))
+        } ~
+        path("ping") {
+          complete("PONG!")
+        } ~
+        path("crash") {
+          sys.error("BOOM!")
+        }
+      }
+    }
+    val host = "localhost"
+    val port = 8080
+    Http().bindAndHandle(route, host, port)
   }
 }

3912b2cf8fd9877f453a720cc6e1e1edd1c9026b
Ryo Ota <nwtgck@gmail.com>
2019-06-11 23:19:22 +0900

    [add] Create hello-world project

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "gh-card"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.11.12"
---
+++ src/main/scala/io/github/nwtgck/gh_card/Main.scala
@@ -0,0 +1,7 @@
+package io.github.nwtgck.gh_card
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("hello, world")
+  }
+}

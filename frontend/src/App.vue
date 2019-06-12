8c306520b968cd54faef97ad45edd4ec9e22d604
Ryo Ota <nwtgck@gmail.com>
2019-06-12 19:25:21 +0900

    [add] Add a scaffold of frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>GitHub Repository Card<br>for every web site</h1>
+    <HelloWorld />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

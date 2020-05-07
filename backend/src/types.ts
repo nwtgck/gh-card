9d097c2f092fdee3f6075698748cc07ab2001607
Freyskeyd <simon.paitrault@gmail.com>
2020-05-07 13:40:59 +0200

    fix #226
    Signed-off-by: Freyskeyd <simon.paitrault@gmail.com>

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -4,7 +4,7 @@ import * as t from 'io-ts';
 // NOTE: You can define fields more if need
 //       (see: https://developer.github.com/v3/repos/#response-5)
 export const githubRepoJsonType = t.type({
-  description: t.union([t.string, t.undefined]),
+  description: t.union([t.string, t.undefined, t.null]),
   language: t.union([t.string, t.null]),
   stargazers_count: t.number,
   forks_count: t.number,
